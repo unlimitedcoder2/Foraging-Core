c673e453f7f75586d2c4bd92d2013a914172829b
Tech <debitcardzpaypal@gmail.com>
2021-02-25 20:43:29 -0500

    refactor: removed a bunch of warnings

--- src/main/java/me/tech/foraging/player/events/RegionEnterEvent.java
+++ src/main/java/me/tech/foraging/player/events/RegionEnterEvent.java
@@ -1,12 +1,9 @@
 package me.tech.foraging.player.events;
 
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import me.tech.foraging.Foraging;
 import net.raidstone.wgevents.events.RegionsChangedEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import java.util.Set;
 
 public class RegionEnterEvent implements Listener {
 	private final Foraging foraging;
@@ -17,7 +14,7 @@ public class RegionEnterEvent implements Listener {
 
 	@EventHandler
 	public void onRegionEnter(RegionsChangedEvent ev) {
-		Set<String> regions = ev.getCurrentRegionsNames();
-
+		// Set<String> regions = ev.getCurrentRegionsNames();
+		this.foraging.getLogger().info("Changed regions!");
 	}
 }
