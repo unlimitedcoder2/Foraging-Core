2aad480cc718162f94a333add16ffbd6ff9f04e6
Tech <debitcardzpaypal@gmail.com>
2021-02-27 23:53:35 -0500

    refactor: implemented multi directory support

--- src/main/java/me/tech/foraging/ConfigManager.java
+++ src/main/java/me/tech/foraging/ConfigManager.java
@@ -1,27 +1,80 @@
 package me.tech.foraging;
 
+import me.tech.foraging.models.player.ForagingPlayerLanguage;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class ConfigManager {
 	private final Foraging foraging;
-
-	private HashMap<String, FileConfiguration> configurations = new HashMap<>();
+	private final HashMap<String, FileConfiguration> configurations = new HashMap<>();
 
 	public ConfigManager(Foraging foraging) {
 		this.foraging = foraging;
 	}
 
-	public void initConfigs() {
-		File directory = new File(foraging.getDataFolder(), "");
- 		String[] contents = directory.list();
+	/**
+	 * Main configuration loader.
+	 */
+	public void load() {
+		String[] configurations = {"config.yml"};
 
- 		for(String content : contents) {
- 			foraging.getLogger().info(content);
-	    }
+		this.loadLanguages();
 
- 		this.configurations.clear();
+		for(String fileName : configurations) {
+			// Current path of folders to create.
+			List<String> currentPath = new ArrayList<>();
+
+			// Load any and all directories a configuration file may have.
+			if(fileName.contains("/")) {
+				// List of all directories leading to the file.
+				List<String> directoryTree = Arrays.asList(fileName.split("/"));;
+				// Remove the actual config file from the directory list.
+				directoryTree.remove(directoryTree.size() - 1);
+				for(String directory : directoryTree) {
+					// Add current directory to the path list.
+					currentPath.add(directory);
+					createFolder(String.join("/", currentPath));
+				}
+			}
+
+			String path = String.join("/", currentPath);
+			if(!path.equals("")) path += "/";
+
+			File file = new File(foraging.getDataFolder(), String.format("%s%s", path, fileName));
+
+			if(!file.exists()) foraging.saveResource(String.format("%s%s", path, fileName), false);
+		}
+	}
+
+	/**
+	 * Load all language files.
+	 */
+	protected void loadLanguages() {
+        createFolder("lang");
+        ForagingPlayerLanguage[] supportedLanguages = ForagingPlayerLanguage.values();
+
+        for(ForagingPlayerLanguage language : supportedLanguages) {
+        	File file = new File(foraging.getDataFolder(), String.format("lang/%s.yml", language.getId()));
+
+        	if(!file.exists())
+        		foraging.saveResource(String.format("lang/%s.yml", language.getId()), false);
+        }
+	}
+
+	/**
+	 * Create a directory in the data folder.
+	 * @param path
+	 */
+	protected void createFolder(String path) {
+		File file = new File(foraging.getDataFolder(), path);
+		if(!file.exists()) {
+			file.mkdir();
+			foraging.getLogger().info(String.format("Created directory %s", path));
+		}
 	}
 }
