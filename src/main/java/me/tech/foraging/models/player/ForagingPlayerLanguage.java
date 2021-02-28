dbfddd9d0432aa3d65df09ebd5dc319e17a2936b
Tech <debitcardzpaypal@gmail.com>
2021-02-27 23:53:11 -0500

    feat: added file id to languages

--- src/main/java/me/tech/foraging/models/player/ForagingPlayerLanguage.java
+++ src/main/java/me/tech/foraging/models/player/ForagingPlayerLanguage.java
@@ -1,15 +1,21 @@
 package me.tech.foraging.models.player;
 
 public enum ForagingPlayerLanguage {
-	en("English");
+	en("English", "en_us");
 
 	private final String name;
+	private final String id;
 
-	ForagingPlayerLanguage(String name) {
+	ForagingPlayerLanguage(String name, String id) {
 		this.name = name;
+		this.id = id;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public String getId() {
+		return id;
 	}
 }
