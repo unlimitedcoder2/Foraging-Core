6df1ce4dbc47518089b51a3548058231a2c7c7b0
Tech <debitcardzpaypal@gmail.com>
2021-02-27 19:38:25 -0500

    fix: now passing foraging through constructor

---
+++ src/main/java/me/tech/foraging/items/ItemManager.java
@@ -0,0 +1,19 @@
+package me.tech.foraging.items;
+
+import me.tech.foraging.Foraging;
+
+import java.util.HashMap;
+
+public class ItemManager {
+	private final Foraging foraging;
+
+	private HashMap<String, ForagingItem> items = new HashMap<>();
+
+	public ItemManager(Foraging foraging) {
+		this.foraging = foraging;
+	}
+
+	public void initItems() {
+		this.items.clear();
+	}
+}
